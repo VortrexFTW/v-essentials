298215d68b6efc7900cc8cd398094868d53ae5aa
Vortrex <adrianbram@gmail.com>
2020-03-11 17:49:59 -0500

    Added obj_pos command

--- sandbox/client/object.js
+++ sandbox/client/object.js
@@ -31,3 +31,55 @@ addCommandHandler("obj", function(cmdName, params) {
 
 // ----------------------------------------------------------------------------
 
+addCommandHandler("obj_pos", function(cmdName, params) {
+	if(isParamsInvalid(params)) {
+		message("/obj_pos <abs/rel> <x> <y> <z>", syntaxMessageColour);
+		return false;
+	}
+	
+	let objects = getObjectsFromParams(splitParams[0]);
+	let positionType = splitParams[1] || "rel";
+	let positionX = splitParams[2] || 0.0;
+	let positionY = splitParams[3] || 0.0;
+	let positionZ = splitParams[4] || 0.0;
+	
+	let newPosition = new Vec3(positionX, positionY, positionZ);
+	
+	let outputText = "";
+	
+	if(objects.length == 0) {
+		message("No objects found!", errorMessageColour);
+		return false;
+	}	
+	
+	if(newPosition == null) {
+		message("The new position is invalid! One of the coordinates may be wrong!", errorMessageColour);
+		return false;		
+	}
+	
+	let isAbsolute = false;
+	
+	if(positionType == "abs" || positionType == "absolute") {
+		isAbsolute = true;
+	}
+	
+	if(isConnected) {
+		triggerNetworkEvent("sb.o.pos", objects, isAbsolute, newPosition.x, newPosition.y, newPosition.z);
+	} else {
+		objects.forEach(function(object) {
+			let currentPosition = object.position;
+			
+			object.position = new Vec3(currentPosition.x+newPosition.x, currentPosition.y+newPosition.y, currentPosition.z+newPosition.z);
+		});
+	}
+
+	if(objects.length > 1) {
+		outputText = "set the position of " + String(objects.length) + " objects to " + String(newPosition.x) + ", " + String(newPosition.y) + ", " + String(newPosition.z) + " " + String((isAbsolute) ? "(absolute)" : "(relative)");
+	} else {
+		outputText = "set the position of " + getProperObjectPossessionText(splitParams[0]) + " to " + String(newPosition.x) + ", " + String(newPosition.y) + ", " + String(newPosition.z) + " " + String((isAbsolute) ? "(absolute)" : "(relative)");
+	}
+	
+	outputSandboxMessage(outputText);
+
+	return true;
+});
