b56c4359645f68278b15c80a656a650b49778c8c
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2021-04-17 16:30:03 -0500

    Use arrow funcs, remove collision stuff

--- v-afk/client.js
+++ v-afk/client.js
@@ -1,46 +1,28 @@
 "use strict";
 
+let isFocused = true;
+
 // ----------------------------------------------------------------------------
 
 setErrorMode(RESOURCEERRORMODE_STRICT);
 
 // ----------------------------------------------------------------------------
 
-addEventHandler("OnLostFocus", function(event) {
+addEventHandler("OnLostFocus", (event) => {
+	isFocused = false;
 	triggerNetworkEvent("v.afk", true);
-	if(localPlayer != null) {
-		localPlayer.collisionsEnabled = false;
-		if(gta.game <= GAME_GTA_VC) {
-			localPlayer.alpha = 100;
-		}
-		gta.setCiviliansEnabled(false);
-		gta.setTrafficEnabled(false);
-
-		//getElementsByType(ELEMENT_CIVILIAN).filter(c => c.isSyncer).forEach((c2) => { destroyElement(c2) });
-		//getElementsByType(ELEMENT_VEHICLE).filter(v => v.isSyncer).forEach((v2) => { destroyElement(v2) });
-	}
 });
 
 // ----------------------------------------------------------------------------
 
-addEventHandler("OnFocus", function(event) {
-	if(localPlayer != null) {
-		//if(localClient.getData("v.afk") == 1) {
-			localPlayer.collisionsEnabled = true;
-			if(gta.game <= GAME_GTA_VC) {
-				localPlayer.alpha = 255;
-			}
-			triggerNetworkEvent("v.afk", false);
-
-			gta.setCiviliansEnabled(true);
-			gta.setTrafficEnabled(true);		
-		//}
-	}
+addEventHandler("OnFocus", (event) => {
+	isFocused = true;
+	triggerNetworkEvent("v.afk", false);
 });
 
 // ----------------------------------------------------------------------------
 
-addEventHandler("OnMouseMove", function(event, mouseId, absolute, position) {
+addEventHandler("OnMouseMove", (event, mouseId, absolute, position) => {
 	if(localClient.getData("v.afk") > 0) {
 		triggerNetworkEvent("v.afk", false);
 	}
@@ -48,8 +30,10 @@ addEventHandler("OnMouseMove", function(event, mouseId, absolute, position) {
 
 // ----------------------------------------------------------------------------
 
-addEventHandler("OnKeyDown", function(event, virtualKey, physicalKey, keyModifiers) {
+addEventHandler("OnKeyDown", (event, virtualKey, physicalKey, keyModifiers) => {
 	if(localClient.getData("v.afk") > 0) {
 		triggerNetworkEvent("v.afk", false);
 	}
 });
+
+// ----------------------------------------------------------------------------
