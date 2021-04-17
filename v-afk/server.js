e3737a0b84fa0fbc0e7c8603f35556caba03c748
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2021-04-17 16:29:47 -0500

    Use arrow funcs

--- v-afk/server.js
+++ v-afk/server.js
@@ -9,25 +9,25 @@ addEvent("OnPlayerBackFromAFK", 1);
 
 // ----------------------------------------------------------------------------
 
-addEventHandler("OnPlayerJoined", function(event, client) {
+addEventHandler("OnPlayerJoined", (event, client) => {
 	client.setData("v.afk", 0, true);
 });
 
 // ----------------------------------------------------------------------------
 
-bindEventHandler("OnResourceStart", thisResource, function(event, client) {
+bindEventHandler("OnResourceStart", thisResource, (event, client) => {
 	let clients = getClients();
 	for(let i in clients) {
 		if(clients[i].getData("v.afk") == null) {
 			clients[i].setData("v.afk", 0, true);
-			
+
 		}
 	}
 });
 
 // ----------------------------------------------------------------------------
 
-addNetworkHandler("v.afk", function(client, state) {
+addNetworkHandler("v.afk", (client, state) => {
 	if(state == true) {
 		client.setData("v.afk", 1, true);
 		if(client.player != null) {
@@ -45,14 +45,14 @@ addNetworkHandler("v.afk", function(client, state) {
 
 // ----------------------------------------------------------------------------
 
-addCommandHandler("afk", function(command, params, client) {
+addCommandHandler("afk", (command, params, client) => {
 	if(client.getData("v.afk") > 0) {
 		client.setData("v.afk", 0, true);
-		message(client.name + " is no longer AFK", COLOUR_YELLOW);
+		message(`${client.name} is no longer AFK`, COLOUR_YELLOW);
 		triggerEvent("OnPlayerBackFromAFK", client);
 	} else {
 		client.setData("v.afk", 2, true);
-		message(client.name + " is now AFK", COLOUR_YELLOW);		
+		message(`${client.name} is now AFK`, COLOUR_YELLOW);
 		triggerEvent("OnPlayerGoAFK", client);
 	}
 });
