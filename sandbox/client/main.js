6fab28b979f3fdf6c7e875e8f12b8b151d06696e
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2021-04-17 16:33:20 -0500

    Init IV stuff

--- sandbox/client/main.js
+++ sandbox/client/main.js
@@ -14,6 +14,25 @@ bindEventHandler("OnResourceStart", thisResource, function(event, resource) {
 	} else {
 		resyncWorld();
 	}
+
+	if(gta.game == GAME_GTA_IV) {
+		if(gta.ivGamemode == 8 || gta.ivGamemode == 30) {
+			natives.allowEmergencyServices(true);
+			natives.setCreateRandomCops(true);
+			natives.setMaxWantedLevel(0);
+			natives.setWantedMultiplier(0.0);
+			natives.allowPlayerToCarryNonMissionObjects(natives.getPlayerId(), true);
+
+			natives.setPlayerTeam(natives.getPlayerId(), 0);
+			natives.usePlayerColourInsteadOfTeamColour(true);
+		}
+		natives.requestAnims("DANCING");
+		natives.loadAllObjectsNow();
+
+		gta.startNewScript("ambnightclubm92");
+		gta.startNewScript("drinking");
+		bindKey(SDLK_m, KEYSTATE_UP, function() { natives.activateNetworkSettingsMenu(); });
+	}
 });
 
 // ----------------------------------------------------------------------------
@@ -39,4 +58,3 @@ function outputSandboxMessage(messageText) {
 }
 
 // ----------------------------------------------------------------------------
-
