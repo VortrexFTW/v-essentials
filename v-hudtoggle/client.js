c686814bbd9f36eac0a56beebed0d45c157e7065
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2021-04-17 16:28:11 -0500

    Add F8 key to toggle GTAC chatbox

--- v-hudtoggle/client.js
+++ v-hudtoggle/client.js
@@ -7,6 +7,7 @@ setErrorMode(RESOURCEERRORMODE_STRICT);
 // ----------------------------------------------------------------------------
 
 let hudEnabled = true;
+let chatEnabled = false;
 
 // ----------------------------------------------------------------------------
 
@@ -14,12 +15,14 @@ bindEventHandler("OnResourceStart", thisResource, function(event, resource) {
 	setHUDEnabled(hudEnabled);
 	setChatWindowEnabled(hudEnabled);
 	bindKey(SDLK_F7, KEYSTATE_UP, toggleHUD);
+	bindKey(SDLK_F8, KEYSTATE_UP, toggleChatBox);
 });
 
 // ----------------------------------------------------------------------------
 
 bindEventHandler("OnResourceStop", thisResource, function(event, resource) {
 	unbindKey(SDLK_F7);
+	unbindKey(SDLK_F8);
 });
 
 // ----------------------------------------------------------------------------
@@ -27,7 +30,13 @@ bindEventHandler("OnResourceStop", thisResource, function(event, resource) {
 function toggleHUD() {
 	hudEnabled = !hudEnabled;
 	setHUDEnabled(hudEnabled);
-	setChatWindowEnabled(hudEnabled);
+}
+
+// ----------------------------------------------------------------------------
+
+function toggleChatBox() {
+	chatEnabled = !chatEnabled;
+	setChatWindowEnabled(chatEnabled);
 }
 
 // ----------------------------------------------------------------------------
