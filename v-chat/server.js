01716f2607e761f724872cec1791575bfde7599c
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2022-01-27 11:04:31 -0600

    Move translate stuff to v-chat

--- v-chat/server.js
+++ v-chat/server.js
@@ -9,6 +9,187 @@ let scriptConfig = null;
 let errorMessageColour = toColour(237, 67, 55, 255);
 let syntaxMessageColour = toColour(200, 200, 200, 255);
 let actionColour = toColour(177, 156, 217, 255);
+let defaultLanguageId = 28;
+
+// ----------------------------------------------------------------------------
+
+let translateURL = "http://api.mymemory.translated.net/get?de={3}&q={0}&langpair={1}|{2}";
+
+// ----------------------------------------------------------------------------
+
+let translationLanguages = [
+	["Abkhazian", "AB"],
+	["Afar", "AA"],
+	["Afrikaans", "AF", "🇿🇦"],
+	["Albanian", "SQ"],
+	["Amharic", "AM"],
+	["Arabic", "AR"],
+	["Armenian", "HY"],
+	["Assamese", "AS"],
+	["Aymara", "AY"],
+	["Azerbaijani", "AZ"],
+	["Bashkir", "BA"],
+	["Basque", "EU"],
+	["Bengali, Bangla", "BN"],
+	["Bhutani", "DZ"],
+	["Bihari", "BH"],
+	["Bislama", "BI"],
+	["Breton", "BR"],
+	["Bulgarian", "BG"],
+	["Burmese", "MY"],
+	["Byelorussian", "BE"],
+	["Cambodian", "KM"],
+	["Catalan", "CA"],
+	["Chinese", "ZH"],
+	["Corsican", "CO"],
+	["Croatian", "HR"],
+	["Czech", "CS"],
+	["Danish", "DA"],
+	["Dutch", "NL", "🇳🇱"],
+	["English", "EN", "🇺🇸"],
+	["Esperanto", "EO"],
+	["Estonian", "ET"],
+	["Faeroese", "FO"],
+	["Fiji", "FJ"],
+	["Finnish", "FI"],
+	["French", "FR", "🇫🇷"],
+	["Frisian", "FY"],
+	["Gaelic (Scots Gaelic)", "GD"],
+	["Galician", "GL"],
+	["Georgian", "KA"],
+	["German", "DE", "🇩🇪"],
+	["Greek", "EL"],
+	["Greenlandic", "KL"],
+	["Guarani", "GN"],
+	["Gujarati", "GU"],
+	["Hausa", "HA"],
+	["Hebrew", "IW"],
+	["Hindi", "HI"],
+	["Hungarian", "HU"],
+	["Icelandic", "IS"],
+	["Indonesian", "IN"],
+	["Interlingua", "IA"],
+	["Interlingue", "IE"],
+	["Inupiak", "IK"],
+	["Irish", "GA"],
+	["Italian", "IT"],
+	["Japanese", "JA"],
+	["Javanese", "JW"],
+	["Kannada", "KN"],
+	["Kashmiri", "KS"],
+	["Kazakh", "KK"],
+	["Kinyarwanda", "RW"],
+	["Kirghiz", "KY"],
+	["Kirundi", "RN"],
+	["Korean", "KO"],
+	["Kurdish", "KU"],
+	["Laothian", "LO"],
+	["Latin", "LA"],
+	["Latvian, Lettish", "LV"],
+	["Lingala", "LN"],
+	["Lithuanian", "LT"],
+	["Macedonian", "MK"],
+	["Malagasy", "MG"],
+	["Malay", "MS"],
+	["Malayalam", "ML"],
+	["Maltese", "MT"],
+	["Maori", "MI"],
+	["Marathi", "MR"],
+	["Moldavian", "MO"],
+	["Mongolian", "MN"],
+	["Nauru", "NA"],
+	["Nepali", "NE"],
+	["Norwegian", "NO"],
+	["Occitan", "OC"],
+	["Oriya", "OR"],
+	["Oromo, Afan", "OM"],
+	["Pashto, Pushto", "PS"],
+	["Persian", "FA"],
+	["Polish", "PL", "🇵🇱"],
+	["Portuguese", "PT"],
+	["Portuguese", "PT"],
+	["Punjabi", "PA"],
+	["Quechua", "QU"],
+	["Rhaeto-Romance", "RM"],
+	["Romanian", "RO"],
+	["Russian", "RU", "🇷🇺"],
+	["Samoan", "SM"],
+	["Sangro", "SG"],
+	["Sanskrit", "SA"],
+	["Serbian", "SR"],
+	["Serbo-Croatian", "SH"],
+	["Sesotho", "ST"],
+	["Setswana", "TN"],
+	["Shona", "SN"],
+	["Sindhi", "SD"],
+	["Singhalese", "SI"],
+	["Siswati", "SS"],
+	["Slovak", "SK", "🇸🇰"],
+	["Slovenian", "SL", "🇸🇮"],
+	["Somali", "SO", "🇸🇴"],
+	["Spanish", "ES", "🇪🇸"],
+	["Sudanese", "SU"],
+	["Swahili", "SW"],
+	["Swedish", "SV"],
+	["Tagalog", "TL"],
+	["Tajik", "TG"],
+	["Tamil", "TA"],
+	["Tatar", "TT"],
+	["Tegulu", "TE"],
+	["Thai", "TH"],
+	["Tibetan", "BO"],
+	["Tigrinya", "TI"],
+	["Tonga", "TO"],
+	["Tsonga", "TS"],
+	["Turkish", "TR"],
+	["Turkmen", "TK"],
+	["Twi", "TW"],
+	["Ukrainian", "UK"],
+	["Urdu", "UR"],
+	["Uzbek", "UZ"],
+	["Vietnamese", "VI"],
+	["Volapuk", "VO"],
+	["Welsh", "CY"],
+	["Wolof", "WO"],
+	["Xhosa", "XH"],
+	["Yiddish", "JI"],
+	["Yoruba", "YO"],
+	["Zulu", "ZU"]
+];
+
+// ----------------------------------------------------------------------------
+
+let countryLanguages = {
+	"DE": "DE",
+	"BR": "PT",
+	"PT": "PT",
+	"RU": "RU",
+	"FR": "FR",
+	"EG": "AR",
+	"IQ": "AR",
+	"RO": "RO",
+	"PL": "PL",
+	"CZ": "CS",
+	"CN": "ZH",
+	"ES": "ES",
+	"MX": "ES",
+	"CO": "ES",
+};
+
+// ----------------------------------------------------------------------------
+
+// Translation Cache
+let cachedTranslations = new Array(translationLanguages.length);
+let cachedTranslationFrom = new Array(translationLanguages.length);
+cachedTranslationFrom.fill([]);
+cachedTranslations.fill(cachedTranslationFrom);
+
+// ----------------------------------------------------------------------------
+
+addEventHandler("OnPlayerJoined", function(event, client) {
+	let languageId = getPlayerLanguage(client.name);
+	client.setData("v.translate", languageId);
+});
 
 // ----------------------------------------------------------------------------
 
@@ -21,12 +202,24 @@ bindEventHandler("OnResourceStart", thisResource, (event, client) => {
 		return false;
 	}
 
-	console.log(`[V.CHAT] Resource started! (Emoji ${(scriptConfig.enableEmoji) ? "enabled" : "disabled"})`);
+	defaultLanguageId = getLanguageIdFromParams(scriptConfig.defaultLanguage) || 28;
+
+	getClients().forEach(function(client) {
+		let languageId = getPlayerLanguage(client.name);
+		client.setData("v.translate", languageId);
+	});
+
+	runTranslatorTest();
+
+	console.log(`[V.CHAT] Resource started! (Emoji ${(scriptConfig.enableEmoji) ? "enabled" : "disabled"}, Translator ${(scriptConfig.enableTranslator) ? "enabled" : "disabled"})`);
 });
 
 // ----------------------------------------------------------------------------
 
 bindEventHandler("OnResourceStop", thisResource, (event, resource) => {
+	getClients().forEach(function(client) {
+		client.removeData("v.translate");
+	});
 	console.log("[V.CHAT] Resource stopped!");
 });
 
@@ -50,7 +243,7 @@ addCommandHandler("talk", (cmdName, params, client) => {
 
 // ----------------------------------------------------------------------------
 
-addEventHandler("OnPlayerChat", (event, client, messageText) => {
+addEventHandler("OnPlayerChat", async (event, client, messageText) => {
 	//Make sure the default chat doesn't show up (this resource customizes chat messages!)
 	event.preventDefault();
 
@@ -63,7 +256,15 @@ addEventHandler("OnPlayerChat", (event, client, messageText) => {
 		messageText = replaceEmojiInString(messageText);
 	}
 
-	message(`${client.name}: [#FFFFFF]${messageText}`, colour);
+	let clients = getClients();
+	for(let i in clients) {
+		let translatedText;
+		translatedText = await translateMessage(messageText, client.getData("v.translate"), clients[i].getData("v.translate"));
+
+		let original = (client.getData("v.translate") == clients[i].getData("v.translate")) ? `` : ` [#AAAAAA](${messageText})`;
+		messageClient(`${client.name}: [#FFFFFF]${translatedText}${original}`, clients[i], colour);
+	}
+
 	console.log(`${client.name}: ${messageText}`)
 });
 
@@ -77,6 +278,135 @@ function replaceEmojiInString(messageText) {
 		return messageText;
 	}
 	return null;
+}
+
+// ----------------------------------------------------------------------------
+
+function getLanguageIdFromParams(params) {
+	// Search locale abbreviations first (en, es, de, etc)
+	for(let i in translationLanguages) {
+		if(translationLanguages[i][1].toLowerCase().indexOf(params.toLowerCase()) != -1) {
+			return Number(i);
+		}
+	}
+
+	// Search english-based language names next (English, Spanish, German, etc)
+	for(let i in translationLanguages) {
+		if(translationLanguages[i][0].toLowerCase().indexOf(params.toLowerCase()) != -1) {
+			return Number(i);
+		}
+	}
+
+	return false;
+}
+
+// ----------------------------------------------------------------------------
+
+addCommandHandler("lang", async function(command, params, client) {
+	if(!params) {
+		messageClient(`/${command} <language name>`, client, syntaxMessageColour);
+		return false;
+	}
+
+	let languageId = getLanguageIdFromParams(params);
+	if(!languageId) {
+		messageClient("That language was not found!", client, errorMessageColour);
+		return false;
+	}
+
+	let tempLanguageId = client.getData("v.translate");
+	client.setData("v.translate", languageId);
+	setPlayerLanguage(client.name, languageId);
+
+	let outputString = "Your language has been set to " + translationLanguages[languageId][0];
+	let translatedMessage = await translateMessage(outputString, getLanguageIdFromParams("EN"), languageId);
+	console.log(translatedMessage);
+	messageClient(translatedMessage, client, COLOUR_YELLOW);
+});
+
+// ----------------------------------------------------------------------------
+
+function getPlayerLanguage(name) {
+	/*
+	let ini = module.ini.create();
+	ini.loadFile("translate.ini");
+	let languageId = ini.getIntValue("Game", name, 28);
+	console.log("LANG: " + languageId);
+	module.ini.delete(ini);
+
+	return languageId;
+	*/
+	return 28;
+}
+
+// ----------------------------------------------------------------------------
+
+function setPlayerLanguage(name, languageId) {
+	/*
+	let ini = module.ini.create();
+	ini.loadFile("translate.ini");
+	ini.setIntValue("Game", name, languageId);
+	module.ini.delete(ini);
+
+	return languageId;
+	*/
+}
+
+// ----------------------------------------------------------------------------
+
+async function translateMessage(messageText, translateFrom = defaultLanguageId, translateTo = defaultLanguageId) {
+	if(translateFrom == translateTo) {
+		return messageText;
+	}
+
+	return new Promise(resolve => {
+		for(let i in cachedTranslations[translateFrom][translateTo]) {
+			if(cachedTranslations[translateFrom][translateTo][0] == messageText) {
+				console.log("[Translate]: Using existing translation for " + translationLanguages[translateFrom][0] + " to " + translationLanguages[translateTo][0] + " - (" + messageText + "), (" + cachedTranslations[translateFrom][translateTo][1] + ")");
+				resolve(cachedTranslations[translateFrom][translateTo][1]);
+			}
+		}
+
+		let thisTranslationURL = translateURL.format(encodeURI(messageText), translationLanguages[translateFrom][1], translationLanguages[translateTo][1], scriptConfig.emailAddress);
+		httpGet(
+			thisTranslationURL,
+			"",
+			function(data) {
+				data = ab2str(data);
+				//tdata = data.substr(0, data.lastIndexOf("}")+1);
+				let translationData = JSON.parse(data);
+				cachedTranslations[translateFrom][translateTo].push([messageText, translationData.responseData.translatedText]);
+				resolve(translationData.responseData.translatedText);
+			},
+			function(data) {
+			}
+		);
+	});
+}
+
+// ----------------------------------------------------------------------------
+
+String.prototype.format = function() {
+	let a = this;
+	for(let i in arguments) {
+		a = a.replace("{" + String(i) + "}", arguments[i]);
+	}
+	return a;
+}
+
+// ----------------------------------------------------------------------------
+
+async function runTranslatorTest() {
+	let translateTo = getLanguageIdFromParams("ES");
+	console.log(translateTo);
+	let translateTest = await translateMessage("Hello", defaultLanguageId, translateTo);
+	console.log("[Translate] Testing translator (EN/ES): Hello / " + String(translateTest));
+}
+
+// ----------------------------------------------------------------------------
+
+function ab2str(buf) {
+	return String.fromCharCode.apply(null, new Uint8Array(buf));
 }
 
 // ----------------------------------------------------------------------------
