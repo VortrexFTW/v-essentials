bb7cd087dda8017e3bd69f8f218ac5e65a4f33a5
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2022-07-11 04:56:09 -0500

    Fix server names resource

--- v-servernames/server.js
+++ v-servernames/server.js
@@ -2,31 +2,50 @@
 
 // ===========================================================================
 
+let serverName = "";
 let nameTimer = null;
+let scriptConfig = null;
+
+// ===========================================================================
+
 bindEventHandler("OnResourceStart", thisResource, (event, resource) => {
 	let scriptConfigFile = loadTextFile("config.json");
-	if(scriptConfigFile == null || scriptConfigFile == "") {
-		thisResource.stop();
+	if (scriptConfigFile == null || scriptConfigFile == "") {
+		event.preventDefault();
 		return false;
 	}
 
-	if(scriptConfigFile == null || scriptConfigFile == "") {
-		scriptConfig = JSON.parse(scriptConfigFile);
+	if (scriptConfigFile == null || scriptConfigFile == "") {
+		event.preventDefault();
 		return false;
 	}
 
-    serverName = server.name;
-    nameTimer = setInterval(() => {
-        let randomName = getRandom(0, scriptConfig.randomNames.length-1);
-        server.name = `${serverName} - ${scriptConfig.randomNames[randomName]}`;
-    }, changeInterval);
+	scriptConfig = JSON.parse(scriptConfigFile);
+
+	if (scriptConfig == null) {
+		event.preventDefault();
+		return false;
+	}
+
+	serverName = server.name;
+	nameTimer = setInterval(() => {
+		let randomName = getRandom(0, scriptConfig.randomNames.length - 1);
+		server.name = `${serverName} - ${scriptConfig.randomNames[randomName]}`;
+	}, scriptConfig.changeInterval);
 });
+
+// ===========================================================================
 
 bindEventHandler("OnResourceStop", thisResource, (event, resource) => {
-    clearInterval(nameTimer);
-    server.name = serverName;
-    collectAllGarbage();
+	if (nameTimer != null) {
+		clearInterval(nameTimer);
+	}
+
+	server.name = serverName;
+	collectAllGarbage();
 });
+
+// ===========================================================================
 
 function getRandom(min, max) {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
