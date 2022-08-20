41ca6dec5fb8fd9bae41eeb87a0318d1adfeb44c
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2022-08-20 12:46:09 -0500

    Add spawn protection resource

---
+++ v-spawnprotect/client.js
@@ -0,0 +1,48 @@
+"use strict";
+
+let spawnProtectPosition = new Vec3(0.0, 0.0, 0.0);
+let spawnProtectRadius = 100.0;
+
+// ----------------------------------------------------------------------------
+
+if (gta.game == GAME_GTA_III) {
+	spawnProtectPosition = new Vec3(139.54, -903.00, 26.16);
+	spawnProtectRadius = 100.0;
+} else if (gta.game == GAME_GTA_VC) {
+	spawnProtectPosition = new Vec3(-379.16, -535.27, 17.28);
+	spawnProtectRadius = 100.0;
+} else if (gta.game == GAME_GTA_SA) {
+	spawnScreenPosition = new Vec3(2495.03, -1685.66, 13.51);
+	spawnProtectRadius = 100.0;
+} else if (gta.game == GAME_GTA_IV || gta.game == GAME_GTA_IV_EFLC) {
+	spawnProtectPosition = new Vec3(-1166.20, 1873.51, 6.648);
+	spawnProtectRadius = 100.0;
+}
+
+// ----------------------------------------------------------------------------
+
+addEventHandler("OnProcess", function (event, deltaTime) {
+	if (localPlayer == null) {
+		return false;
+	}
+
+	if (gta.game == GAME_GTA_IV || gta.game == GAME_GTA_IV_EFLC) {
+		if (gta.ivGamemode == 8) {
+			if (localPlayer.position.distance(spawnProtectPosition) <= spawnProtectRadius) {
+				//localPlayer.invincible = true;
+				natives.setCharProofs(localPlayer, true, true, true, true, true);
+			} else {
+				//localPlayer.invincible = false;
+				natives.setCharProofs(localPlayer, false, false, false, false, false);
+			}
+		}
+	} else {
+		if (localPlayer.position.distance(spawnProtectPosition) <= spawnProtectRadius) {
+			localPlayer.invincible = true;
+		} else {
+			localPlayer.invincible = false;
+		}
+	}
+});
+
+// ----------------------------------------------------------------------------
