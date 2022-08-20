154a2a2e932b6faf132b6ea33f4e81d61c1f01bb
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2022-08-20 12:41:38 -0500

    Hide chatbox when pressing TAB

--- v-hudtoggle/client.js
+++ v-hudtoggle/client.js
@@ -2,27 +2,28 @@
 
 // ----------------------------------------------------------------------------
 
-setErrorMode(RESOURCEERRORMODE_STRICT);
-
-// ----------------------------------------------------------------------------
-
 let hudEnabled = true;
-let chatEnabled = false;
+let chatEnabled = true;
+let chatHidden = false;
 
 // ----------------------------------------------------------------------------
 
-bindEventHandler("OnResourceStart", thisResource, function(event, resource) {
+bindEventHandler("OnResourceStart", thisResource, function (event, resource) {
 	setHUDEnabled(hudEnabled);
-	setChatWindowEnabled(hudEnabled);
+	setChatWindowEnabled(chatEnabled);
 	bindKey(SDLK_F7, KEYSTATE_UP, toggleHUD);
 	bindKey(SDLK_F8, KEYSTATE_UP, toggleChatBox);
+	bindKey(SDLK_TAB, KEYSTATE_DOWN, hideChatBox);
+	bindKey(SDLK_TAB, KEYSTATE_UP, showChatBox);
 });
 
 // ----------------------------------------------------------------------------
 
-bindEventHandler("OnResourceStop", thisResource, function(event, resource) {
+bindEventHandler("OnResourceStop", thisResource, function (event, resource) {
 	unbindKey(SDLK_F7);
 	unbindKey(SDLK_F8);
+
+	unbindKey(SDLK_TAB);
 });
 
 // ----------------------------------------------------------------------------
@@ -30,6 +31,13 @@ bindEventHandler("OnResourceStop", thisResource, function(event, resource) {
 function toggleHUD() {
 	hudEnabled = !hudEnabled;
 	setHUDEnabled(hudEnabled);
+	if (game.game == GAME_GTA_IV) {
+		natives.displayCash(hudEnabled);
+		natives.displayAmmo(hudEnabled);
+		natives.displayHud(hudEnabled);
+		natives.displayRadar(hudEnabled);
+		natives.displayAreaName(hudEnabled);
+	}
 }
 
 // ----------------------------------------------------------------------------
@@ -37,6 +45,20 @@ function toggleHUD() {
 function toggleChatBox() {
 	chatEnabled = !chatEnabled;
 	setChatWindowEnabled(chatEnabled);
+}
+
+// ----------------------------------------------------------------------------
+
+function hideChatBox() {
+	chatEnabled = true;
+	setChatWindowEnabled(false);
+}
+
+// ----------------------------------------------------------------------------
+
+function showChatBox() {
+	chatHidden = false;
+	setChatWindowEnabled(true);
 }
 
 // ----------------------------------------------------------------------------
