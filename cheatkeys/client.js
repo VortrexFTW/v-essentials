80bfb9c588762c534e356a1b57652c76142197cf
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2022-08-20 12:39:06 -0500

    Remove error mode

--- cheatkeys/client.js
+++ cheatkeys/client.js
@@ -1,22 +1,17 @@
 "use strict";
 
-setErrorMode(RESOURCEERRORMODE_STRICT);
-
-function GetTargetPhysical()
-{
+function GetTargetPhysical() {
 	let vehicle = localPlayer.vehicle;
 	if (vehicle != null)
 		return vehicle;
 	return localPlayer;
 }
 
-function SpeedKey(physical,multiplier)
-{
+function SpeedKey(physical, multiplier) {
 	if (physical == null)
 		physical = GetTargetPhysical();
 
-	if (physical.isSyncer)
-	{
+	if (physical.isSyncer) {
 		// set train speed
 		if (physical.isType(ELEMENT_TRAIN))
 			physical.speed *= multiplier;
@@ -35,13 +30,11 @@ function SpeedKey(physical,multiplier)
 	}
 }
 
-function VehicleJump(physical)
-{
+function VehicleJump(physical) {
 	if (physical == null)
 		physical = GetTargetPhysical();
 
-	if (physical.isSyncer)
-	{
+	if (physical.isSyncer) {
 		if (physical.isType(ELEMENT_TRAIN))
 			physical.derailed = true;
 
@@ -51,10 +44,10 @@ function VehicleJump(physical)
 		if (gta.game == GAME_GTA_IV || gta.game == GAME_GTA_IV_EFLC)
 			multiplier = 20.0;
 		let velocity = physical.velocity;
-		velocity[0] += mat.getElement(1*4+0)*0.3*multiplier;
-		velocity[1] += mat.getElement(1*4+1)*0.3*multiplier;
-		velocity[2] += mat.getElement(1*4+2)*0.3*multiplier;
-		velocity[2] += 0.7*multiplier;
+		velocity[0] += mat.getElement(1 * 4 + 0) * 0.3 * multiplier;
+		velocity[1] += mat.getElement(1 * 4 + 1) * 0.3 * multiplier;
+		velocity[2] += mat.getElement(1 * 4 + 2) * 0.3 * multiplier;
+		velocity[2] += 0.7 * multiplier;
 		physical.velocity = velocity;
 	}
 }
@@ -64,44 +57,40 @@ bindKey(SDLK_7, KEYSTATE_DOWN, (key) => {
 		VehicleJump();
 }, true);
 
-addEventHandler("OnKeyDown",(event,keyCode,scanCode,mod) => {
-	switch (keyCode)
-	{
+addEventHandler("OnKeyDown", (event, keyCode, scanCode, mod) => {
+	switch (keyCode) {
 		case SDLK_F8:
 			{
 				let Peds = getPeds();
-				for (let i=0; i<Peds.length; i++)
+				for (let i = 0; i < Peds.length; i++)
 					if (Peds[i] != localPlayer)
 						destroyElement(Peds[i]);
 
 				let Vehicles = getVehicles();
-				for (let i=0; i<Vehicles.length; i++)
+				for (let i = 0; i < Vehicles.length; i++)
 					if (localPlayer == null || Vehicles[i] != localPlayer.vehicle)
 						destroyElement(Vehicles[i]);
 			}
 			break;
 		case SDLK_2:
 			if (localPlayer != null)
-				SpeedKey(null,2);
+				SpeedKey(null, 2);
 			break;
 		case SDLK_3:
 			if (localPlayer != null)
-				SpeedKey(null,0);
+				SpeedKey(null, 0);
 			break;
 		case SDLK_5:
 			if (localPlayer != null)
-				SpeedKey(null,-1);
+				SpeedKey(null, -1);
 			break;
 		case SDLK_6:
 			{
-				if (localPlayer != null)
-				{
+				if (localPlayer != null) {
 					localPlayer.health = 100;
 					let vehicle = localPlayer.vehicle;
-					if (vehicle != null && vehicle.isSyncer)
-					{
-						if (vehicle.isType(ELEMENT_TRAIN))
-						{
+					if (vehicle != null && vehicle.isSyncer) {
+						if (vehicle.isType(ELEMENT_TRAIN)) {
 							vehicle.speed = 0;
 							vehicle.derailed = false;
 						}
