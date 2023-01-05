bb9b88bb40960d33cbd5b2b39625346d0a0fe6b0
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2023-01-05 13:56:48 -0600

    Remove old game init stuff

--- v-sandbox/client/main.js
+++ v-sandbox/client/main.js
@@ -5,48 +5,6 @@ let gameName = gameNames[game.game];
 
 // ----------------------------------------------------------------------------
 
-bindEventHandler("OnResourceStart", thisResource, function (event, resource) {
-	if (isConnected) {
-		triggerNetworkEvent("sb.clientready");
-		triggerNetworkEvent("sb.w.sync");
-	} else {
-		resyncWorld();
-	}
-
-	if (game.game == GAME_GTA_IV) {
-		if (game.ivGamemode == 8 || game.ivGamemode == 30) {
-			natives.allowEmergencyServices(true);
-			natives.setCreateRandomCops(true);
-			natives.setMaxWantedLevel(0);
-			natives.setWantedMultiplier(0.0);
-			natives.allowPlayerToCarryNonMissionObjects(natives.getPlayerId(), true);
-			natives.switchRandomTrains(true);
-			natives.switchRandomBoats(true);
-			natives.switchAmbientPlanes(true);
-			natives.switchMadDrivers(false);
-			natives.setPlayerTeam(natives.getPlayerId(), 0);
-			natives.usePlayerColourInsteadOfTeamColour(true);
-			natives.allowGameToPauseForStreaming(false);
-		}
-		natives.requestAnims("DANCING");
-		natives.loadAllObjectsNow();
-
-		//game.startNewScript("ambnightclubm92");
-		//game.startNewScript("drinking");
-		//bindKey(SDLK_m, KEYSTATE_UP, function() { natives.activateNetworkSettingsMenu(); });
-	}
-
-	godMode = false;
-	localPlayer.invincible = godMode;
-	if (game.game < GAME_GTA_IV) {
-		localPlayer.setProofs(godMode, godMode, godMode, godMode, godMode);
-	} else if (game.game == GAME_GTA_IV) {
-		natives.setCharProofs(localPlayer, godMode, godMode, godMode, godMode, godMode);
-	}
-});
-
-// ----------------------------------------------------------------------------
-
 bindEventHandler("OnResourceStop", thisResource, function (event, resource) {
 	//unbindKey(SDLK_m);
 });
