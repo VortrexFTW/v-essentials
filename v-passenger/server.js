8096156eba7e725a558325ae151f08f8b584544f
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2023-01-05 13:56:05 -0600

    Remove server script file for v-passenger

--- v-passenger/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-// ----------------------------------------------------------------------------
-
-addNetworkHandler("v.enterveh", function(client, vehicle, driver) {
-	triggerNetworkEvent("v.enterveh", null, client.player.id, vehicle.id, driver);
-}
-
-// ----------------------------------------------------------------------------
