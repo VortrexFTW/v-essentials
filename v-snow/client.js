480114d5866f06033f636ff8a7dcb486a165632c
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2023-12-13 05:13:56 -0600

    Update snow resource

--- v-snow/client.js
+++ v-snow/client.js
@@ -2,9 +2,46 @@
 
 // ===========================================================================
 
-addNetworkHandler("v.snow", (fallingSnow, groundSnow) => {
-	snowing = fallingSnow;
-	snow.enabled = groundSnow;
+addNetworkHandler("v.snow", (fallingSnow, groundSnow, forceSnow, heavySnow, snowConfetti, snowBumpiness) => {
+	if (snowing != fallingSnow) {
+		snowing = fallingSnow;
+	}
+
+	if (snow.enabled != groundSnow) {
+		snow.enabled = groundSnow;
+	}
+
+	if (snow.confetti != snowConfetti) {
+		snow.confetti = snowConfetti;
+	}
+
+	if (snow.bumpiness != snowBumpiness) {
+		snow.bumpiness = snowBumpiness;
+	}
+
+	if (heavySnow == true) {
+		if (snowing) {
+			snow.addFlakes();
+		}
+	} else {
+		snow.clearFlakes();
+	}
+
+	if (snow.forced != forceSnow) {
+		forceSnowing(forceSnow);
+	}
+});
+
+// ===========================================================================
+
+bindEventHandler("OnResourceStart", thisResource, (event, resource) => {
+	if (server.game < GAME_GTA_III || server.game > GAME_GTA_IV) {
+		console.warn("Snow is not supported on this game! Resource stopping ...");
+		event.preventDefault();
+	}
+
+	// Ask server for snow settings
+	triggerNetworkEvent("v.snow");
 });
 
 // ===========================================================================
--- v-snow/server.js
+++ v-snow/server.js
@@ -1,20 +1,110 @@
 "use strict";
 
+let fallingSnow = false;
+let groundSnow = false;
+let forceSnow = false;
+let heavySnow = false;
+let snowConfetti = false;
+let snowBumpiness = 1.0;
+
 // ===========================================================================
 
 bindEventHandler("OnResourceStart", thisResource, (event, resource) => {
-    if (server.game < GAME_GTA_III || server.game > GAME_GTA_SA) {
+    if (server.game < GAME_GTA_III || server.game > GAME_GTA_IV) {
         console.warn("Snow is not supported on this game! Resource stopping ...");
         event.preventDefault();
     }
 
-    triggerNetworkEvent("v.snow", null, !!server.getCVar("fallingsnow"), !!server.getCVar("groundsnow"));
+    fallingSnow = !!server.getCVar("FallingSnow");
+    groundSnow = !!server.getCVar("GroundSnow");
+    forceSnow = !!server.getCVar("ForceSnow");
+    heavySnow = !!server.getCVar("HeavySnow");
+    snowConfetti = !!server.getCVar("SnowConfetti");
+    snowBumpiness = server.getCVar("SnowBumpiness");
+
+    updateClientSnow(null);
 });
 
 // ===========================================================================
 
 addEventHandler("OnPlayerJoined", (event, client) => {
-    triggerNetworkEvent("v.snow", client, !!server.getCVar("fallingsnow"), !!server.getCVar("groundsnow"));
+    updateClientSnow(client);
 });
 
 // ===========================================================================
+
+addNetworkHandler("v.snow", (event, client) => {
+    updateClientSnow(client);
+});
+
+// ===========================================================================
+
+addCommandHandler("fallingsnow", (command, params, client) => {
+    if (server.getCVar("SnowAdmin")) {
+        if (client.administrator || client.console) {
+            fallingSnow = !fallingSnow;
+            updateClientSnow(null);
+        }
+    }
+});
+
+// ===========================================================================
+
+addCommandHandler("groundsnow", (command, params, client) => {
+    if (server.getCVar("SnowAdmin")) {
+        if (client.administrator || client.console) {
+            groundSnow = !groundSnow;
+            updateClientSnow(null);
+        }
+    }
+});
+
+// ===========================================================================
+
+addCommandHandler("forcesnow", (command, params, client) => {
+    if (server.getCVar("SnowAdmin")) {
+        if (client.administrator || client.console) {
+            forceSnow = !forceSnow;
+            updateClientSnow(null);
+        }
+    }
+});
+
+// ===========================================================================
+
+addCommandHandler("heavysnow", (command, params, client) => {
+    if (server.getCVar("SnowAdmin")) {
+        if (client.administrator || client.console) {
+            heavySnow = !heavySnow;
+            updateClientSnow(null);
+        }
+    }
+});
+
+// ===========================================================================
+
+addCommandHandler("confettisnow", (command, params, client) => {
+    if (server.getCVar("SnowAdmin")) {
+        if (client.administrator || client.console) {
+            snowConfetti = !snowConfetti;
+            updateClientSnow(null);
+        }
+    }
+});
+
+// ===========================================================================
+
+addCommandHandler("snowbumpiness", (command, params, client) => {
+    if (server.getCVar("SnowAdmin")) {
+        if (client.administrator || client.console) {
+            snowBumpiness = parseFloat(params);
+            updateClientSnow(null);
+        }
+    }
+});
+
+// ===========================================================================
+
+function updateClientSnow(client) {
+    triggerNetworkEvent("v.snow", client, fallingSnow, groundSnow, forceSnow, heavySnow, snowConfetti, snowBumpiness);
+}
