3bfe856a905b67e13bcaff03f2fdc4918d4c67ab
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2023-12-19 00:19:25 -0600

    Fix snow command access

--- v-snow/server.js
+++ v-snow/server.js
@@ -40,67 +40,79 @@ addNetworkHandler("v.snow", (event, client) => {
 // ===========================================================================
 
 addCommandHandler("fallingsnow", (command, params, client) => {
-    if (server.getCVar("SnowAdmin")) {
-        if (client.administrator || client.console) {
-            fallingSnow = !fallingSnow;
-            updateClientSnow(null);
+    if (!!server.getCVar("SnowAdmin")) {
+        if (!client.administrator && !client.console) {
+            return false;
         }
     }
+
+    fallingSnow = !fallingSnow;
+    updateClientSnow(null);
 });
 
 // ===========================================================================
 
 addCommandHandler("groundsnow", (command, params, client) => {
-    if (server.getCVar("SnowAdmin")) {
-        if (client.administrator || client.console) {
-            groundSnow = !groundSnow;
-            updateClientSnow(null);
+    if (!!server.getCVar("SnowAdmin")) {
+        if (!client.administrator && !client.console) {
+            return false;
         }
     }
+
+    groundSnow = !groundSnow;
+    updateClientSnow(null);
 });
 
 // ===========================================================================
 
 addCommandHandler("forcesnow", (command, params, client) => {
-    if (server.getCVar("SnowAdmin")) {
-        if (client.administrator || client.console) {
-            forceSnow = !forceSnow;
-            updateClientSnow(null);
+    if (!!server.getCVar("SnowAdmin")) {
+        if (!client.administrator && !client.console) {
+            return false;
         }
     }
+
+    forceSnow = !forceSnow;
+    updateClientSnow(null);
 });
 
 // ===========================================================================
 
 addCommandHandler("heavysnow", (command, params, client) => {
-    if (server.getCVar("SnowAdmin")) {
-        if (client.administrator || client.console) {
-            heavySnow = !heavySnow;
-            updateClientSnow(null);
+    if (!!server.getCVar("SnowAdmin")) {
+        if (!client.administrator && !client.console) {
+            return false;
         }
     }
+
+    heavySnow = !heavySnow;
+    updateClientSnow(null);
 });
 
 // ===========================================================================
 
 addCommandHandler("confettisnow", (command, params, client) => {
-    if (server.getCVar("SnowAdmin")) {
-        if (client.administrator || client.console) {
-            snowConfetti = !snowConfetti;
-            updateClientSnow(null);
+    if (!!server.getCVar("SnowAdmin")) {
+        if (!client.administrator && !client.console) {
+            return false;
         }
     }
+
+    snowConfetti = !snowConfetti;
+    updateClientSnow(null);
 });
 
 // ===========================================================================
 
 addCommandHandler("snowbumpiness", (command, params, client) => {
-    if (server.getCVar("SnowAdmin")) {
-        if (client.administrator || client.console) {
-            snowBumpiness = parseFloat(params);
-            updateClientSnow(null);
+    if (!!server.getCVar("SnowAdmin")) {
+        if (!client.administrator && !client.console) {
+            return false;
         }
     }
+
+    snowBumpiness = parseFloat(params);
+    updateClientSnow(null);
 });
 
 // ===========================================================================
@@ -108,3 +120,5 @@ addCommandHandler("snowbumpiness", (command, params, client) => {
 function updateClientSnow(client) {
     triggerNetworkEvent("v.snow", client, fallingSnow, groundSnow, forceSnow, heavySnow, snowConfetti, snowBumpiness);
 }
+
+// ===========================================================================
