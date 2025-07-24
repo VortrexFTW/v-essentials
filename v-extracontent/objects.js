a5eb77d2d11801db60784a3e8b35eef1d4ac6bf5
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2025-07-24 07:13:09 -0500

    Add objects lists

---
+++ v-extracontent/objects.js
@@ -0,0 +1,33 @@
+// ===========================================================================
+// Connected Roleplay - Extra Content (GTA SA)
+// (c) 2024 Connected Roleplay
+// ===========================================================================
+// FILE: objects.js
+// DESC: Extra custom content for Connected Roleplay's GTA VC server
+// TYPE: Client (JavaScript)
+// ===========================================================================
+
+"use strict";
+
+// ===========================================================================
+
+let serverObjectGroups = [
+];
+
+// ===========================================================================
+
+let serverObjects = [
+];
+
+// ===========================================================================
+
+let serverGates = [
+	// name, modelId, positionX, positionY, positionZ, rotationX, rotationY, rotationZ, openPositionX, openPositionY, openPositionZ, openRotationX, openRotationY, openRotationZ, dimension = -1, interior = 0, extra = {}
+];
+
+// ===========================================================================
+
+let serverSyncedGates = [
+];
+
+// ===========================================================================
