2006aefdf90361f40aaac815ef3ab84a392dc10a
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2026-02-19 08:29:15 -0600

    Check if connected for map load

--- v-spawn/client.js
+++ v-spawn/client.js
@@ -3,11 +3,12 @@
 // ----------------------------------------------------------------------------
 
 addEventHandler("OnMapLoaded", function (event, mapName) {
-	if (!isConnected) {
+	if (isConnected) {
 		triggerNetworkEvent("OnMapLoaded", mapName);
 	} else {
 		game.createPlayer(mapSpawnPoints[mapName], 0.0, spawnSkins[game.game]);
 	}
 });
+
 
 // ----------------------------------------------------------------------------
