136fed26beb4b69467057be2bc93a413f638c582
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2026-04-02 02:36:45 -0500

    Fix for IV passenger

--- v-passenger/client.js
+++ v-passenger/client.js
@@ -3,6 +3,12 @@
 // ----------------------------------------------------------------------------
 
 bindEventHandler("OnResourceStart", thisResource, function(event, resource) {
+	if(game.game == 10) {
+		console.warn(`[${thisResource.name}] This resource does not work on Mafia 1, which has it's own passenger entry mechanism`);
+		event.preventDefault();
+		return false;
+	}
+
 	bindKey(SDLK_g, KEYSTATE_UP, enterVehicleAsPassenger);
 });
 
@@ -14,19 +20,46 @@ bindEventHandler("OnResourceStop", thisResource, function(event, resource) {
 
 // ----------------------------------------------------------------------------
 
-function enterVehicleAsPassenger() {
-	if(localPlayer.vehicle == null) {
-		let tempVehicle = getClosestVehicle(localPlayer.position);
-		if(tempVehicle != null) {
+// In case other resources want to use this
+exportFunction("enterPassenger", enterVehicleAsPassenger);
+addNetworkHandler("v.passenger", function(pedId) { enterVehicleAsPassenger(getElementFromId(pedId))});
+
+// ----------------------------------------------------------------------------
+
+function enterVehicleAsPassenger(ped = localPlayer) {
+	if(ped == null) {
+		return false;
+	}
+
+	if(ped.vehicle == null) {
+		let vehicle = getClosestVehicle(localPlayer.position);
+		if(vehicle != null) {
 			if(game.game == GAME_GTA_SA) {
+				// GTA SA crashes when entering as passenger, just warp directly into the car instead
 				for (let seatIndex = 1; seatIndex < 8; seatIndex++) { //seat index starts from 1
-					if (tempVehicle.getOccupant(seatIndex) === null) {
-						localPlayer.warpIntoVehicle(tempVehicle, seatIndex);
+					if (vehicle.getOccupant(seatIndex) === null) {
+						localPlayer.warpIntoVehicle(vehicle, seatIndex);
 						break;
 					}
 				}
-			} else {
-				localPlayer.enterVehicle(tempVehicle, false);
+			} else if(game.game <= GAME_GTA_VC) {
+				localPlayer.enterVehicle(vehicle, false);
+			} else if(game.game == GAME_GTA_IV) {
+				let seatOffsets = [
+					new Vec3(1.0, 0.0, 0.0),
+					new Vec3(-1.0, -0.3, 0.0),
+					new Vec3(1.0, -0.3, 0.0)
+				];
+
+				let closestSeat = seatOffsets.filter((offset, i) => {
+					console.log(`Checking seat ${i+1}`);
+					natives.isCarPassengerSeatFree(vehicle, i+1)})
+				.reduce((i, j) => {
+					(natives.getOffsetFromCarInWorldCoords(vehicle, seatOffsets.findIndex(seat => seat == i)+1).distance(ped.position) < natives.getOffsetFromCarInWorldCoords(vehicle, seatOffsets.findIndex(seat => seat == j)+1).distance(ped.position)) ? i : j
+				});
+
+				natives.disablePlayerAutoVehicleExit(ped, true);
+				natives.taskEnterCarAsPassenger(ped, vehicle, 5000, seatOffsets.findIndex(seat => seat == closestSeat) + 1);
 			}
 		}
 	}
@@ -35,7 +68,7 @@ function enterVehicleAsPassenger() {
 // ----------------------------------------------------------------------------
 
 function getClosestVehicle(pos) {
-    return getVehicles().reduce((i, j) => (i.position.distance(pos) < j.position.distance(pos)) ? i : j);
+	return getElementsByType(ELEMENT_VEHICLE).reduce((i, j) => (i.position.distance(pos) < j.position.distance(pos)) ? i : j);
 }
 
 // ----------------------------------------------------------------------------
